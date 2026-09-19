732de5e4f9ccc8752f2df574a49ede1f47e504e4
Linus Kåreblom <linus.kareblom@gmail.com>
2026-09-19 13:23:09 +0200

    nvim: Scala support

--- neovim/init.lua
+++ neovim/init.lua
@@ -60,6 +60,19 @@ vim.api.nvim_create_user_command("Now", function()
 end, {})
 
 --------------------------------------------------------------------------------
+-- Autocmd
+--------------------------------------------------------------------------------
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "scala",
+    callback = function()
+        vim.bo.shiftwidth = 4
+        vim.bo.tabstop = 4
+        vim.bo.expandtab = true
+    end,
+})
+
+--------------------------------------------------------------------------------
 -- Lazy
 --------------------------------------------------------------------------------
 
@@ -468,6 +481,31 @@ require("lazy").setup({
     {
         "j-hui/fidget.nvim",
         opts = {},
+    },
+    {
+        "scalameta/nvim-metals",
+        ft = { "scala", "sbt", "java" },
+        opts = function()
+            local metals_config = require("metals").bare_config()
+
+            metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            metals_config.on_attach = function(client, bufnr)
+                -- require("metals").setup_dap()
+            end
+
+            return metals_config
+        end,
+        config = function(self, metals_config)
+            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = self.ft,
+                callback = function()
+                    require("metals").initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+            })
+        end
     }
 }, {
     checker = { enabled = true }
